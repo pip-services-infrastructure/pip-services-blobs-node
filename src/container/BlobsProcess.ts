bdad5b032f6a68c286e8e88b56b3ad25294dcaee
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-10 15:44:03 +0300

    added pip-services-oss

--- src/container/BlobsProcess.ts
+++ src/container/BlobsProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { BlobsServiceFactory } from '../build/BlobsServiceFactory';
 
@@ -8,6 +9,7 @@ export class BlobsProcess extends ProcessContainer {
     public constructor() {
         super("blobs", "Blobs microservice");
         this._factories.add(new BlobsServiceFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 }
