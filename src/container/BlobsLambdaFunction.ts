362b51d6edd1d0522d6e7371abd6f73b6916d5e1
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-13 19:07:14 +0300

    added elsaticsearch,prometheus,heartbeat

--- src/container/BlobsLambdaFunction.ts
+++ src/container/BlobsLambdaFunction.ts
@@ -1,5 +1,8 @@
 import { Descriptor } from 'pip-services-commons-node';
 import { CommandableLambdaFunction } from 'pip-services-aws-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
+
 import { BlobsServiceFactory } from '../build/BlobsServiceFactory';
 
 export class BlobsLambdaFunction extends CommandableLambdaFunction {
@@ -7,6 +10,8 @@ export class BlobsLambdaFunction extends CommandableLambdaFunction {
         super("blobs", "Blobs function");
         this._dependencyResolver.put('controller', new Descriptor('pip-services-blobs', 'controller', 'default', '*', '*'));
         this._factories.add(new BlobsServiceFactory());
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 }
 
--- src/container/BlobsProcess.ts
+++ src/container/BlobsProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
 import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { BlobsServiceFactory } from '../build/BlobsServiceFactory';
@@ -9,6 +10,7 @@ export class BlobsProcess extends ProcessContainer {
     public constructor() {
         super("blobs", "Blobs microservice");
         this._factories.add(new BlobsServiceFactory);
+        this._factories.add(new DefaultNetFactory);
         this._factories.add(new DefaultOssFactory);
     }
 
