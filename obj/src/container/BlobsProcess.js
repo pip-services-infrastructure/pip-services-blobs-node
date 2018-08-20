bdcff923dcc1501752b4b9db82c806ef5b7f74c8
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:30:49 +0300

    added DefaultRpcFactory

--- obj/src/container/BlobsProcess.js
+++ obj/src/container/BlobsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const BlobsServiceFactory_1 = require("../build/BlobsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class BlobsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("blobs", "Blobs microservice");
         this._factories.add(new BlobsServiceFactory_1.BlobsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.BlobsProcess = BlobsProcess;
--- src/container/BlobsProcess.ts
+++ src/container/BlobsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { BlobsServiceFactory } from '../build/BlobsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class BlobsProcess extends ProcessContainer {
 
     public constructor() {
         super("blobs", "Blobs microservice");
         this._factories.add(new BlobsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
